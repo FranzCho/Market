f88ba2077ba284e4f9b39c73c1cddfd3a209f4a9
Franz Cho <franzzcho@gmail.com>
2023-10-15 00:21:32 +0900

    feat: 로그인 & 회원가입 구현

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,20 +1,27 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import { User } from "@prisma/client";
 
-const NavItem = ({ mobile }: { mobile?: boolean }) => {
-  const { data: session, status } = useSession();
-  console.log({ session }, status);
+interface NavItemProps {
+  mobile?: boolean;
+  currentUser?: User | null;
+}
 
+const NavItem = ({ mobile, currentUser }: NavItemProps) => {
   return (
-    <ul className={`text-md justify-center flex gap-4 w-full items-center ${mobile && "flex-col h-full"}`}>
+    <ul
+      className={`text-md justify-center flex gap-4 w-full items-center ${
+        mobile && "flex-col h-full"
+      }`}
+    >
       <li className="py-2 text-center border-b-4 cursor-pointer">
         <Link href="/admin">Admin</Link>
       </li>
       <li className="py-2 text-center border-b-4 cursor-pointer">
         <Link href="/user">User</Link>
       </li>
-      {session?.user ? (
+      {currentUser ? (
         <li className="py-2 text-center border-b-4 cursor-pointer">
           <Link href="/admin">
             <button onClick={() => signOut()}>SignOut</button>
