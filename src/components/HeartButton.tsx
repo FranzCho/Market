5a71c2dfede6b5d010370435ea0b58f08a04a7e4
Franz Cho <franzzcho@gmail.com>
2023-11-06 18:20:56 +0900

    feat: done

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import React from "react";
+import useFavorite from "@/hooks/useFavorite";
 
 interface HeartButtonProps {
   productId: string;
@@ -8,13 +9,24 @@ interface HeartButtonProps {
 }
 
 const HeartButton = ({ productId, currentUser }: HeartButtonProps) => {
+  const { hasFavorite, toggleFavorite } = useFavorite({
+    productId,
+    currentUser,
+  });
+
   return (
-    <div className="relative transition cursor-pointer hover:opacity-80">
+    <div
+      onClick={toggleFavorite}
+      className="relative transition cursor-pointer hover:opacity-80"
+    >
       <AiOutlineHeart
         size={28}
         className="absolute fill-white -top-[2px] -right-[2px]"
       />
-      <AiFillHeart size={24} className={"fill-rose-500"} />
+      <AiFillHeart
+        size={24}
+        className={hasFavorite ? "fill-rose-500" : "fill-neutral-500"}
+      />
     </div>
   );
 };
