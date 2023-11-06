060cb090dd55db4139709f7b66f7e26e4f34a0b8
Franz Cho <franzzcho@gmail.com>
2023-11-06 23:15:41 +0900

    feat: react-toastify 추가

---
+++ src/components/ToastProvider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
+const ToastProvider = () => {
+  return (
+    <div>
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+};
+
+export default ToastProvider;
